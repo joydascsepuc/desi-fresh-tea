d469759d4822d4904b1372009142e1d7d95f6bd9
JoyDas <joydascsepuc@gmail.com>
2021-01-13 17:02:19 +0600

    footer template

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,61 @@
+<footer class="footer">
+	<div class="row">
+		<div class="col-8" style="background-color: #525659; color: white;">
+			logo
+			<hr>
+			<div class="row">
+				<div class="col-md-4">
+					<h4>About Us</h4>
+					<p>Deshi Fresh Tea is dedicated to produce high quality fresh tea for you.</p>
+				</div>
+				<div class="col-md-4">
+					<h4>Contact Us</h4>
+					<p>House No-706, Bailjuri, Turag, Dhaka-1230, Bangladesh.</p>
+					<p>info@deshifreshtea.com.bd</p>
+					<p>(+880) - 1984237868</p>
+				</div>
+				<div class="col-md-4">
+					<h4>About Us</h4>
+					<div class="row">
+						<div class="col-6">
+							<a href="#">Blog</a>
+							<br>
+							<a href="#">BSTI</a>
+							<br>
+							<a href="#">FAQ</a>
+						</div>
+						<div class="col-6">
+							<a href="#">Web Mail</a>
+							<br>
+							<a href="#">Admin Login</a>
+							<br>
+							<a href="#">Downloads</a>
+							<br>
+							<a href="#">Brand</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br>
+			<hr>
+			<br>
+			<pre class="text-right"><h4>1 0 2 0 1 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4></pre>
+			<br>
+			<p class="text-right">Copyright @ Desi Fresh Tea | All Rights Reserved &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+		</div>
+		<div class="col-4" style="background-color: blue;">
+			<h2 class="text-right">(+880) - 1918 390 119</h2>
+			<br>
+			<br>
+			<a href="">Facebook Link</a>
+			<br>
+			<br>
+			<br>
+			<h4>Developed By - Hotchpotch Innovations Limited</h4>
+		</div>
+	</div>
+</footer>
+
+{{-- <section class="vertical">
+	<div class="separator"></div>
+</section> --}}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+/*Pages*/
+Route::get('/', 'App\Http\Controllers\PagesController@index')->name('homePage');
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
